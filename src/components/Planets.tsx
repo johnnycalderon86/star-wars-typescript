9995e649320a1ab721559e4be1a74e0ac2cab999
Johnny <johnnycalderondeveloper@gmail.com>
2020-09-16 18:33:42 +0200

    fetching planets

--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import {useQuery} from 'react-query'
-
-const getPlanets = ()
+import { useQuery } from 'react-query'
 
 
- const Planets = () => {
-    const {} = useQuery('planets', getPlanets)
 
+
+
+const getPlanets = async () => {
+    const endpoint = 'https://swapi.dev/api/planets';
+
+    const request = await (await fetch(endpoint)).json();
+    return request
+
+}
+
+
+const Planets = (): JSX.Element => {
+    const { data, status } = useQuery('planets', getPlanets)
+    console.log(data, status);
+    
 
     return (
         <div>
-        <h1>Planets</h1>
+            <h1>Planets</h1>
+            {status}
         </div>
     )
 }
